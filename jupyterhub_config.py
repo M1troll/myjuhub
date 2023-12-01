b8c4ffaadb9103b4a3990dab863be3eade7e0d2a
Anton Oboleninov <anton.oboleninov@saritasa.com>
2023-12-01 20:39:29 +0700

    --wip-- [skip ci]

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -7,6 +7,6 @@ c.JupyterHub.hub_ip = '127.0.0.1'
 
 # Users
 c.Authenticator.whitelist = {'sar1tasa', 'tmp_user'}
-c.Authenticator.admin_users = {'sar1tasa', 'tmp_user'}
+c.Authenticator.admin_users = {'sar1tasa'}
 
 c.LocalAuthenticator.create_system_users = True
