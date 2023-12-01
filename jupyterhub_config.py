901db68d79711a56eddf096ec4e32e00e256efd3
Anton Oboleninov <anton.oboleninov@saritasa.com>
2023-12-01 19:55:58 +0700

    --wip-- [skip ci]

---
+++ jupyterhub_config.py
@@ -0,0 +1,12 @@
+import os
+
+c = get_config()
+
+c.Spawner.args = ["--allow-root"]
+c.JupyterHub.hub_ip = '127.0.0.1'
+
+# Users
+c.Authenticator.whitelist = {'sar1tasa', 'tmp_user'}
+c.Authenticator.admin_users = {'sar1tasa', 'tmp_user'}
+
+c.LocalAuthenticator.create_system_users = True
--- jupyterhub_configefqwe.py
+++
@@ -1,7 +0,0 @@
-import os
-
-c = get_config()
-
-c.JupyterHub.hub_ip = '127.0.0.1'
-c.LocalAuthenticator.create_system_users = True
-
