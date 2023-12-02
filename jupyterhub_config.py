a89b4c4ea019bce5dcfe26f3c4a7e560809c3639
Anton Oboleninov <anton.oboleninov@saritasa.com>
2023-12-02 09:36:22 +0700

    --wip-- [skip ci]

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -6,7 +6,9 @@ c.Spawner.args = ["--allow-root"]
 c.JupyterHub.hub_ip = '127.0.0.1'
 
 # Users
-c.Authenticator.whitelist = {'sar1tasa', 'tmp_user'}
-c.Authenticator.admin_users = {'sar1tasa'}
-
+c.Authenticator.admin_users = {'sar1tasa', 'tmp'}
+c.PAMAuthenticator.admin_groups = {'sar1tasa'}
 c.LocalAuthenticator.create_system_users = True
+
+c.JupyterHub.authenticator_class = "dummy"
+c.DummyAuthenticator.password = "123"
