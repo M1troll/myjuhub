d96bf9114d1f1104e53db4038dab3415e00f9b7a
Anton Oboleninov <anton.oboleninov@saritasa.com>
2023-12-02 11:08:17 +0700

    --wip-- [skip ci]

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -3,12 +3,17 @@ import os
 c = get_config()
 
 c.Spawner.args = ["--allow-root"]
+c.Spawner.cmd = "/home/sar1tasa/.pyenv/versions/myjuhub/bin/jupyterhub-singleuser"
 c.JupyterHub.hub_ip = '127.0.0.1'
 
 # Users
 c.Authenticator.admin_users = {'sar1tasa', 'tmp'}
-c.PAMAuthenticator.admin_groups = {'sar1tasa'}
+c.PAMAuthenticator.admin_groups = {'myjuhub'}
+c.LocalAuthenticator.group_whitelist = ['myjuhub']
+
 c.LocalAuthenticator.create_system_users = True
+
+c.PAMAuthenticator.open_sessions = False
 
 c.JupyterHub.authenticator_class = "dummy"
 c.DummyAuthenticator.password = "123"
